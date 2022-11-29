07ebe8e827d2adc69c2d47b57bee2eff5226e9b9
Matias Gaete <matiasgf18@gmail.com>
2022-11-29 16:14:50 -0300

    feat: added user model and controller

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    email { "MyString" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    password_hash { "MyString" }
+    password_salt { "MyString" }
+  end
+end
